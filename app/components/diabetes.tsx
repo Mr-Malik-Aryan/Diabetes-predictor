53cd48c7726fec7b7b8c60a66b84323db6459d93
Aryan Malik <121257483+Mr-Malik-Aryan@users.noreply.github.com>
2024-11-06 20:31:12 +0530

    les go !

--- app/components/diabetes.tsx
+++ app/components/diabetes.tsx
@@ -37,7 +37,7 @@ const DiabetesPredictor = () => {
         setError(null);
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://diabetesapi-2knl.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
